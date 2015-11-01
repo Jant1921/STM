db479080ceefcd33604d5f89a4e44ff578286a4d
Arphenom <dan20oct@gmail.com>
2015-11-01 15:53:55 -0600

    Insercion en la tabla tipo_accion, tipo_evento,tipo_equipo,continente,formato_evento,torneo,  desde insertar catalogo en la pagina,datos_catalogo.php modificado para  inserciones  respectivas.

--- phpSTM/datos_catalogo.php
+++ phpSTM/datos_catalogo.php
@@ -14,7 +14,66 @@ $conn = new mysqli ( "127.0.0.1:3306", "base1", "base", "stm"); //crea la conexi
 				$stmt->execute ();                             //ejecuta el query     
             }
         }
-  
+        //-------------------------------------------------------------------------------------------------
+        if($catalogo=='tipo_accion'){
+    
+        $scriptAgregar='call insert_tipo_accion(?)'; //guarda en una variable local la instruccion a ejecutar
+          if ($stmt = $conn->prepare ( $scriptAgregar )) { //verifica que la sentencia haya sido preparada para su ejecucion
+				$stmt->bind_param ( 's', $dato );  //define los parametros que recibe la funcion
+				$stmt->execute ();                             //ejecuta el query     
+            }
+        }
+        
+        //--------------------------------------------------------------------------------------------
+        if($catalogo=='tipo_equipo'){
+    
+        $scriptAgregar='call insert_tipo_equipo(?)'; //guarda en una variable local la instruccion a ejecutar
+          if ($stmt = $conn->prepare ( $scriptAgregar )) { //verifica que la sentencia haya sido preparada para su ejecucion
+				$stmt->bind_param ( 's', $dato );  //define los parametros que recibe la funcion
+				$stmt->execute ();                             //ejecuta el query     
+            }
+        }
+        //-------------------------------------------------------------------------------------------------
+        if($catalogo=='tipo_evento'){
+    
+        $scriptAgregar='call Insert_tipo_evento(?)'; //guarda en una variable local la instruccion a ejecutar
+          if ($stmt = $conn->prepare ( $scriptAgregar )) { //verifica que la sentencia haya sido preparada para su ejecucion
+				$stmt->bind_param ( 's', $dato );  //define los parametros que recibe la funcion
+				$stmt->execute ();                             //ejecuta el query     
+            }
+        }
+        
+        //--------------------------------------------------------------------------------------------
+        if($catalogo=='continente'){
+    
+        $scriptAgregar='call insert_continente(?)'; //guarda en una variable local la instruccion a ejecutar
+          if ($stmt = $conn->prepare ( $scriptAgregar )) { //verifica que la sentencia haya sido preparada para su ejecucion
+				$stmt->bind_param ( 's', $dato );  //define los parametros que recibe la funcion
+				$stmt->execute ();                             //ejecuta el query     
+            }
+        }
+        //-------------------------------------------------------------------------------------------------
+        if($catalogo=='formato_evento'){
+    
+        $scriptAgregar='call insert_formato_torneo(?)'; //guarda en una variable local la instruccion a ejecutar
+          if ($stmt = $conn->prepare ( $scriptAgregar )) { //verifica que la sentencia haya sido preparada para su ejecucion
+				$stmt->bind_param ( 's', $dato );  //define los parametros que recibe la funcion
+				$stmt->execute ();                             //ejecuta el query     
+            }
+        }
+        //--------------------------------------------------------------------------------------------
+        if($catalogo=='torneo'){
+    
+        $scriptAgregar='call insert_torneo(?)'; //guarda en una variable local la instruccion a ejecutar
+          if ($stmt = $conn->prepare ( $scriptAgregar )) { //verifica que la sentencia haya sido preparada para su ejecucion
+				$stmt->bind_param ( 's', $dato );  //define los parametros que recibe la funcion
+				$stmt->execute ();                             //ejecuta el query     
+            }
+        }
+        //-------------------------------------------------------------------------------------------------
+        
+        //--------------------------------------------------------------------------------------------
+        header("location: catalogos.html"); // Se redirige al index, con sesion iniciada
       }
   
           
